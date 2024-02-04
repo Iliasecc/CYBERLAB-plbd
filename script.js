5f830213097438e368e18793ceccbbeaf03c318c
Iliasecc <153554669+Iliasecc@users.noreply.github.com>
2024-02-04 23:37:38 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+function toggleHamburgerMenu() {
+    const hamburgerMenu = document.getElementById('hamburger-menu');
+    hamburgerMenu.style.display = hamburgerMenu.style.display === 'none' ? 'block' : 'none';
+}
